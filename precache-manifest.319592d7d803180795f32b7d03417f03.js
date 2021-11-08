61c4b9fd8e526d43038a487ee6d1068678ca5aad
Felix Kolwa <felix.kolwa@plath.de>
2021-11-08 18:56:23 +0100

    Updates

---
+++ precache-manifest.319592d7d803180795f32b7d03417f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6f0142a22889c6a49ea0902eb43c18",
+    "url": "/kolwa/index.html"
+  },
+  {
+    "revision": "572d963ab6084aeb5557",
+    "url": "/kolwa/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2c76a0db4bfb39ef8453",
+    "url": "/kolwa/static/js/2.ae11aa8f.chunk.js"
+  },
+  {
+    "revision": "61747203f8741a6184e2790122ee263b",
+    "url": "/kolwa/static/js/2.ae11aa8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572d963ab6084aeb5557",
+    "url": "/kolwa/static/js/main.8d993429.chunk.js"
+  },
+  {
+    "revision": "ee4c364f0d256644c056",
+    "url": "/kolwa/static/js/runtime-main.d1c33fab.js"
+  }
+]);
