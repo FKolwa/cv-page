ba337cdab28ebd644a49bd7accd24f66adc5b26c
Felix Kolwa <felix.kolwa@plath.de>
2021-11-10 20:00:48 +0100

    WIP

--- .github/workflows/tasks/deploy-gh-page.sh
+++ .github/workflows/tasks/deploy-gh-page.sh
@@ -6,15 +6,15 @@ main_branch="master"
 target_branch="gh-pages"
 build_dir="./build"
 
-cd "$GITHUB_WORKSPACE"
+cd "${GITHUB_WORKSPACE}"
 
 touch CNAME
 echo "${GH_PAGE_CNAME}" >> CNAME
 
-git config user.name "$GITHUB_USER"
+git config user.name "${GITHUB_USER}"
 git config user.email "${GITHUB_USER_EMAIL}"
 
-git add "$build_dir" -f
+git add "${build_dir}" -f
 
 git commit -m "updated GitHub Pages"
 if [ $? -ne 0 ]; then
@@ -22,5 +22,4 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-git remote set-url "$remote_name" "$repo_uri"
-git push --force-with-lease "$remote_name" "$target_branch"
+git push "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/cv-page.git:${target_branch}"
