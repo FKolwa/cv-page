e209d3069435433065ab41651e883d52f8956cbe
Felix Kolwa <felix.kolwa@plath.de>
2021-11-10 11:58:03 +0100

    wip

--- .github/workflows/tasks/deploy-gh-page.sh
+++ .github/workflows/tasks/deploy-gh-page.sh
@@ -2,7 +2,24 @@
 
 set -eu
 
+repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/cv-page.git"
+remote_name="origin"
+main_branch="master"
+target_branch="gh-pages"
+build_dir="/build"
+
+cd "$GITHUB_WORKSPACE"
+
 git config user.name "$GITHUB_USER"
 git config user.email "${GITHUB_USER_EMAIL}"
 
-npm run deploy
+git add "$build_dir"
+
+git commit -m "updated GitHub Pages"
+if [ $? -ne 0 ]; then
+    echo "nothing to commit"
+    exit 0
+fi
+
+git remote set-url "$remote_name" "$repo_uri"
+git push --force-with-lease "$remote_name" "$target_branch"
