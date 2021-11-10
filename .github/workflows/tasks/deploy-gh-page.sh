1016953500c2a02bbc40cda9766de8901684bc83
Felix Kolwa <felix.kolwa@plath.de>
2021-11-10 20:09:13 +0100

    WIP

--- .github/workflows/tasks/deploy-gh-page.sh
+++ .github/workflows/tasks/deploy-gh-page.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/cv-page.git"
+repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/cv-page.git"
 remote_name="origin"
 main_branch="master"
 target_branch="gh-pages"
@@ -8,8 +8,8 @@ build_dir="./build"
 
 cd "${GITHUB_WORKSPACE}"
 
-touch CNAME
-echo "${GH_PAGE_CNAME}" >> CNAME
+touch ./build/CNAME
+echo "${GH_PAGE_CNAME}" >> ./build/CNAME
 
 git config user.name "${GITHUB_USER}"
 git config user.email "${GITHUB_USER_EMAIL}"
@@ -22,4 +22,5 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-git push "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/cv-page.git:${target_branch}"
+git remote set-url "$remote_name" "$repo_uri" # includes access token
+git push --force-with-lease "$remote_name" "$target_branch"
