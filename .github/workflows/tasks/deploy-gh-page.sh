9b4dccb74f25a0b52590037087bcba77f8e2e612
Felix Kolwa <felix.kolwa@plath.de>
2021-11-17 09:55:31 +0100

    wip

--- .github/workflows/tasks/deploy-gh-page.sh
+++ .github/workflows/tasks/deploy-gh-page.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/cv-page.git"
+set -eu
+
+repo_uri="https://x-access-token:${GITHUB_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
-main_branch="master"
+# main_branch="master"
+main_branch="implementing-cd-pipeline" #just for testing
 target_branch="gh-pages"
 build_dir="./build"
 
-cd "${GITHUB_WORKSPACE}"
+cd "$GITHUB_WORKSPACE"
+
+git config user.name "$GITHUB_ACTOR"
+git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
 
-git config user.name "${GITHUB_USER}"
-git config user.email "${GITHUB_USER_EMAIL}"
-
-git checkout "$target_branch"
-git rebase "${remote_name}/${main_branch}"
-
+git checkout "$main_branch"
 npm run build
+npm run deploy
+# git rebase "${remote_name}/${main_branch}"
 
-touch ./build/CNAME
-echo "${GH_PAGE_CNAME}" >> ./build/CNAME
+# git add "$build_dir"
 
-git add "${build_dir}" -f
+# git commit -m "updated GitHub Pages"
+# if [ $? -ne 0 ]; then
+#     echo "nothing to commit"
+#     exit 0
+# fi
 
-git commit -m "Pipeline: updated GitHub Pages"
-if [ $? -ne 0 ]; then
-    echo "nothing to commit"
-    exit 0
-fi
-
-git remote set-url "$remote_name" "$repo_uri" # includes access token
-git push --force-with-lease "$remote_name" "$target_branch"
+# git remote set-url "$remote_name" "$repo_uri" # includes access token
+# git push --force-with-lease "$remote_name" "$target_branch"
